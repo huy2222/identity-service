b7b0920eb04a76b0db69b8520a6aa3c38872871d
huy2222 <nguyennhathuy21112005@gmail.com>
2026-07-24 10:38:39 +0700

    update unique field

--- src/main/java/com/devteria/identityservice/service/UserService.java
+++ src/main/java/com/devteria/identityservice/service/UserService.java
@@ -3,6 +3,7 @@ package com.devteria.identityservice.service;
 import java.util.HashSet;
 import java.util.List;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -34,7 +35,6 @@ public class UserService {
 
     public UserResponse createUser(UserCreationRequest request) {
         log.info("User service create");
-        if (userRepository.existsByUsername(request.getUsername())) throw new AppException(ErrorCode.USER_EXISTED);
         User user = userMapper.toUser(request);
         user.setPassword(passwordEncoder.encode(request.getPassword()));
 
@@ -42,7 +42,14 @@ public class UserService {
         roles.add(Role.USER.name());
 
         user.setRoles(roles);
-        return userMapper.toUserResponse(userRepository.save(user));
+
+        try {
+           user =  userRepository.save(user);
+        } catch (DataIntegrityViolationException e) {
+            throw new AppException(ErrorCode.USER_EXISTED);
+        }
+
+        return userMapper.toUserResponse(user);
     }
 
     public UserResponse updateUser(String userId, UserUpdateRequest request) {
